1b090b604a5c9057798b22ec41519e3dc5e351e8
小青蛙 <lemon-mx@foxmail.com>
2022-05-10 23:34:37 +0800

    增加模块

---
+++ proxy/src/main/java/com/haodoings/haze/xun/App.java
@@ -0,0 +1,13 @@
+package com.haodoings.haze.xun;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
